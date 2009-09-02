e8e5c4ce0c9088a7dc1617dc93b0402f7001122d
Ryo Okamoto <ryo@aquahill.net>
2009-09-02 14:46:12 +0900

    updated /info feature.
    reorganized page element design.
    added 'vocaloid tag' extraction feature.

--- lib/Mikunopop/Info.pm
+++ lib/Mikunopop/Info.pm
@@ -76,7 +76,7 @@ sub handler : method {    ## no critic
 			
 			for my $item( $elem->children ){
 				# get all
-				my @key = qw(title description thumbnail_url first_retrieve length view_counter comment_num mylist_counter watch_url);
+				my @key = qw(title description thumbnail_url first_retrieve length view_counter comment_num mylist_counter watch_url last_res_body);
 				
 				for my $key( @key ){
 					if( $item->name eq $key ){
@@ -84,13 +84,31 @@ sub handler : method {    ## no critic
 					}
 				}
 				
-				# tags
+				# all tag
+				my @tag;
 				if( $item->name eq 'tags' and $item->att('domain') eq 'jp' ){
 					for my $t( $item->children ){
-						$stash->{tag} ||= [];
-						push @{ $stash->{tag} }, $t->trimmed_text;
+						next if $t->trimmed_text eq q{音楽};
+						push @tag, $t->trimmed_text;
 					}
 				}
+				
+				# vocaloid tag
+				my @tag_vocaloid;
+				my @tags = @{ &tags };
+				for my $tag( @tag ){
+					# first が使えない
+					my $flag = 0;
+					for my $t( @tags ){
+						if( $tag =~ /$t/i ){
+							push @tag_vocaloid, $tag;
+						}
+					}
+				}
+				
+				# set
+				$stash->{tag} = [ @tag ];
+				$stash->{tag_vocaloid} = [ @tag_vocaloid ];
 			}
 		};
 		
@@ -151,7 +169,7 @@ sub handler : method {    ## no critic
 
 sub get_pnames {
 	my @p = grep { $_ ne '' } @{ &pnames };
-	my @pn = qw(MikuPOP RinPOP アニメOP ゲームOP エロゲOP 偽OP);    # NG
+	my @pn = qw(MikuPOP RinPOP アニメOP ゲームOP エロゲOP 偽OP J-POP);    # NG
 	
 	my @name;
 	for my $tag(@_){
@@ -187,6 +205,137 @@ sub create_template {
 	};
 
 	return Template->new( $tmpl_config );
+}
+
+# from tags.js
+sub tags {
+return ["",
+#	// 一般
+	"テクノ",
+	"Instrumental",
+	"ChipTune",
+
+#	// 大百科より
+#	// http://dic.nicovideo.jp/a/vocaloid%E9%96%A2%E9%80%A3%E3%81%AE%E3%82%BF%E3%82%B0%E4%B8%80%E8%A6%A7
+	"アングーグラウンドボカロジャパン",
+	"居酒屋KAITO",
+	"カラオケBOX流歌",
+	"キャバレー巡音",
+	"クラブ鏡音",
+	"ジャズクラブ初音",
+	"スナック初音",
+	"全部KAITO",
+	"全部ミク",
+	"全部リン",
+	"全部レン",
+	"Chiptune×VOCALOIDリンク",
+	"電脳愛国婦人",
+	"BAR初音",
+	"BAR弱音",
+	"初音フォークジャンボリー",
+	"初音ミク合唱団シリーズ",
+	"初音ミクの合唱",
+	"初音ミクの弾き語り",
+	"初音洋楽シリーズ",
+	"パブMEIKO",
+	"プログレミク",
+	"VOCAJAZZ",
+	"VOCALOIDアカペラ曲",
+	"VOCALOIDヴィジュ系カバー曲",
+	"VOCALOID合唱団",
+	"VOCALOID口笛",
+	"VOCALOIDミュージカル",
+	"VOCALOID民族調曲",
+	"ボカロ演歌",
+	"ボカロクラシカ",
+	"VOCAROCK",
+	"VOCASKA",
+	"ボカロ童謡",
+	"ボカロメタル",
+	"ボカロメタル殿堂入り",
+	"ボカロラップ",
+	"ミクゲイザー",
+	"ミクトランス",
+	"ミクトロニカ",
+	"ミクノ",
+	"ミクノポップ",
+	"MikuHouse",
+	"ミクHR/HM",
+	"ミク☆バラード",
+	"ミクパンク",
+	"ミクビエント",
+	"MikuPOP",
+	"ミクボッサ",
+	"巡音洋楽シリーズ",
+	"ボカノバ",
+	"ミクメタル",
+	"ミクラシック",
+	"ミクろっく",
+	"ミックンベース",
+	"MEIKO無双",
+	
+#	// ミク
+	"みんなのミクうた",
+	"初音ミク処女作",
+	"切ないミクうた",
+	"爽やかなミクうた",
+	"クールなミクうた",
+	"元気が出るミクうた",
+	"ピアノミク",
+	"隠れたミク名曲",
+	"ミクちゃんといっしょ",
+	"かわいいミクうた",
+	"ききいるミクうた",
+	"ミクのクリスマス曲",
+	"ミクという音源を使ってみた",
+	"初音ミク迷曲リンク",
+	"大人のミク曲",
+	"素朴なミクうた",
+	"ごきげんミクさん",
+	"なごみく",
+	"アコギミク",
+	"元気が減るミクうた",
+	"全部初音ミク",
+	"初音ミクでクラシック",
+	"初音ミクの合唱",
+	"エロかわいいミクうた",
+	"ミクロック",
+
+#	// リン
+	"隠れたリン名曲",
+	"鏡音リン迷曲リンク",
+	"鏡音リン名曲リンク",
+	"みんなのリンうた",
+	"切ないリンうた",
+	"元気が出るリンうた",
+	"爽やかなリンうた",
+	"かわいいリンうた",
+	"クールなリンうた",
+	"恋するリンうた",
+	"ききいるリンうた",
+	"ホットなリンうた",
+	"素朴なリンうた",
+	"元気の出るリンうた",
+
+#	// ルカ
+	"巡音ルカ名曲リンク",
+	"ききいるルカうた",
+	"クールなルカうた",
+	"切ないルカうた",
+	"隠れたルカ名曲",
+	"爽やかなルカうた",
+	"ピアノルカ",
+	"みんなのルカうた",
+	"やさしいルカうた",
+	"かわいいルカうた",
+
+#	// GUMI
+	"かわいいGUMIうた",
+	"ききいるGUMIうた",
+
+#	// 特別
+	"時田トリビュート",
+];
 }
 
 # from pnames.js
@@ -459,8 +608,19 @@ return ["",
 	"ねこぼーろ",
 	"geppei-taro",
 	"グラウル",
+	"yuukiss",
+	"L*aura",
+	"tatmos",
+	"glim",
+	"ねこぼーろ",
+	"geppei-taro",
+	'T@KC',
+	"salome",
+	"おなつ",
+	"サイコパズル",
+	"nyan_nyan",
+	"hiropon_5th",
 ];
-
 }
 
 1;
