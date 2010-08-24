ad0af9fbc31e3c077f9d2cd74395fe29a016a917
Ryo Okamoto <ryo@aquahill.net>
2010-08-25 03:11:46 +0900

    updated some document files.

--- htdocs/js/chat.js
+++ htdocs/js/chat.js
@@ -11,17 +11,29 @@ function loadChat(once) {
 		url: "/var/chat_status.js?" + epoch,
 		dataType: "json",
 		success: function (result, status) {
-			// num
-			if( result.num == 0 ){
-				$('#chat-status').html("今、チャットルームには誰もいません。" );
-			}
-			else{
-				var str = "今、チャットルームに " + result.num + " 名います（" + result.name.join(", ") + "）。" ;
-				if( result.topic != null && result.topic != "" ){
-					str += "トピックは「" + result.topic + "」です。";
+			var user = [];
+			$.each( result.name, function (name, status) {
+				if( status == "active" ){
+					user.push( name );
 				}
-				$('#chat-status').html( str );
-			}
+				else if( status == "inactive" ){
+					user.push( '<span class="dim" title="離席中">' + name + '</span>' );
+				}
+			} );
+			
+			// status
+			var str = ( result.num == 0 )
+				? "今、チャットルームには誰もいません。求むお留守番。"
+				: "今、" + result.num + " 名います（" + user.join(", ") + "）。";
+			
+			$('#chat-status').html( str );
+			
+			// topic
+			var topic = ( result.topic != null && result.topic != "" )
+				? 'トピックは「<span id="chat-topic-title">' + result.topic + '</span>」です。'
+				: "トピックは設定されていません。";
+			
+			$('#chat-topic').html( topic );
 		},
 		error: function (req, status, error) {
 			;
