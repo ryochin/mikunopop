0f7cddab5920d9b9cc82a10c7541d04ae7df737e
Ryo Okamoto <ryo@aquahill.net>
2012-03-01 03:56:32 +0900

    improved mikunopop.user.js.

--- htdocs/userscript/mikunopop.user.js
+++ htdocs/userscript/mikunopop.user.js
@@ -3,21 +3,29 @@
 // @namespace      http://mikunopop.info/
 // @description    Mikunopop Utility Tools
 // @include        http://www.nicovideo.jp/watch/*
-// @version        0.0.1
-// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js
+// @version        0.0.2
 // ==/UserScript==
 
 (function(){
 	var id = location.href.split('/').reverse()[0];
 	var countURL = "http://mikunopop.info/count/" + id;
 	var style = 'margin: auto 4px; color: #cc3333';
+	var showOnlyPlayed = false;    // 彡が０の時は表示させたくないなら true に。
 	
 	function setCount(n) {
+		// check
+		if( n == 0 && showOnlyPlayed == true )
+			return;
+		
+		// span
 		var str = "彡" + n;
-		$("<span>")
-			.text( str )
-			.attr('style', style)
-			.insertBefore('#video_article');
+		var span = document.createElement("span");
+		span.setAttribute("style", style);
+		span.innerHTML = str;
+		
+		// set
+		var article = document.getElementById("video_article");
+		document.getElementById("video_title").insertBefore(span, article);
 	}
 	
 	GM_xmlhttpRequest({
